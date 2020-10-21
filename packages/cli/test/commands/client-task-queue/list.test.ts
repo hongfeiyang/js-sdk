f5d7d44f5f8dc46e12002f41895833e4f6895da2
Josh Bax <josh.bax@meeco.me>
2020-10-21 15:06:44 +1300

    Test default behaviour of client-task-queue:list

--- packages/cli/test/commands/client-task-queue/list.test.ts
+++ packages/cli/test/commands/client-task-queue/list.test.ts
@@ -26,6 +26,18 @@ describe('client-task-queue:list', () => {
     });
 
   customTest
+    .stub(ClientTaskQueueService.prototype, 'list', listEach as any)
+    .stdout()
+    .run(['client-task-queue:list', ...testUserAuth, ...testEnvironmentFile])
+    .it('lists all types of Client Tasks by default', ctx => {
+      const expected = readFileSync(
+        outputFixture('list-client-task-queue-many.output.yaml'),
+        'utf-8'
+      );
+      expect(ctx.stdout.trim()).to.equal(expected.trim());
+    });
+
+  customTest
     .stub(ClientTaskQueueService.prototype, 'listAll', listAll as any)
     .stdout()
     .run([
@@ -68,8 +80,58 @@ const response = {
   meta: [],
 };
 
+const responseEachType = {
+  client_tasks: [
+    {
+      id: 'a',
+      state: ClientTaskState.Todo,
+      work_type: 'update_share',
+      target_id: 'share_item_id_a',
+      additional_options: {},
+      last_state_transition_at: new Date(1),
+      report: {},
+      created_at: new Date(1),
+    },
+    {
+      id: 'b',
+      state: ClientTaskState.InProgress,
+      work_type: 'update_share',
+      target_id: 'share_item_id_b',
+      additional_options: {},
+      last_state_transition_at: new Date(1),
+      report: {},
+      created_at: new Date(1),
+    },
+    {
+      id: 'c',
+      state: ClientTaskState.Done,
+      work_type: 'update_share',
+      target_id: 'share_item_id_b',
+      additional_options: {},
+      last_state_transition_at: new Date(1),
+      report: {},
+      created_at: new Date(1),
+    },
+    {
+      id: 'd',
+      state: ClientTaskState.Failed,
+      work_type: 'update_share',
+      target_id: 'share_item_id_b',
+      additional_options: {},
+      last_state_transition_at: new Date(1),
+      report: {},
+      created_at: new Date(1),
+    },
+  ],
+  meta: [],
+};
+
 function list() {
   return Promise.resolve(response);
+}
+
+function listEach() {
+  return Promise.resolve(responseEachType);
 }
 
 function listAll() {
